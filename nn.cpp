55a7dc8c29826ce360cc3cc8dc4721f589813940
Reece Stevens <reecestevens24@gmail.com>
2017-08-08 14:51:38 -0500

    Limit searching to valid neighboring points

--- nn.cpp
+++ nn.cpp
@@ -11,7 +11,8 @@ typedef boost::geometry::model::point <double, 3, boost::geometry::cs::cartesian
 using namespace spatial_index;
 std::vector<double> *natural_neighbor(std::vector<Point>& known_coordinates,
                                       std::vector<double>& known_values,
-                                      std::vector<Point>& interpolation_points) {
+                                      std::vector<Point>& interpolation_points,
+                                      int coord_max) {
 
     printf("Building KD-Tree\n");
     // Build KD-Tree from known points
@@ -30,29 +31,44 @@ std::vector<double> *natural_neighbor(std::vector<Point>& known_coordinates,
     }
 
     printf("Calculating scattered contributions\n");
+
+    int xscale = coord_max*coord_max;
+    int yscale = coord_max;
+
     // Scatter method discrete Sibson
     for (int i = 0; i < interpolation_points.size(); i++) {
+        if (i%10000 == 0){
+            printf("\tPoint %d of %d\n", i, interpolation_points.size());
+        }
         const QueryResult *q = tree->nearest_iterative(interpolation_points[i]);
-        double r = q->distance;
+        int r = (int) q->distance;
         double comparison_distance = r*r;
-        for (int j = 0; j < interpolation_points.size(); j++) {
-            // Bounding Box Elimination
-            // TODO: this is the performance bottleneck, ways to speed up?
-            double distance_x = interpolation_points[j].get<0>() - interpolation_points[i].get<0>();
-            double distance_y = interpolation_points[j].get<1>() - interpolation_points[i].get<1>();
-            double distance_z = interpolation_points[j].get<2>() - interpolation_points[i].get<2>();
-            if (distance_x*distance_x + distance_y*distance_y
-                    + distance_z*distance_z > comparison_distance){
-                continue;
+        int px = interpolation_points[i].get<0>();
+        int py = interpolation_points[i].get<1>();
+        int pz = interpolation_points[i].get<2>();
+        for (int x = px - r; x < px + r; x++) {
+            if (x < 0) { continue; }
+            for (int y = py - r; y < py + r; y++) {
+                if (y < 0) { continue; }
+                for (int z = pz - r; z < px + r; z++) {
+                    if (z < 0) { continue; }
+                    int idx = x*xscale + y*yscale + z;
+                    double distance_x = interpolation_points[idx].get<0>() - px;
+                    double distance_y = interpolation_points[idx].get<1>() - py;
+                    double distance_z = interpolation_points[idx].get<2>() - pz;
+                    if (distance_x*distance_x + distance_y*distance_y
+                            + distance_z*distance_z > comparison_distance){
+                        continue;
+                    }
+                    accumulator[i] += q->value;
+                    contribution_counter[i] += 1;
+                }
             }
-            accumulator[i] += q->value;
-            contribution_counter[i] += 1;
         }
     }
 
     std::vector<double> interpolation_values(interpolation_points.size());
-
-    printf("Calculating final interpolation values (%d values)\n", interpolation_values.size());
+    printf("Calculating final interpolation values (%d values)\n", (int) interpolation_values.size());
     for (int i = 0; i < interpolation_values.size(); i++) {
         if (contribution_counter[i] != 0) {
             interpolation_values[i] = accumulator[i] / contribution_counter[i];
@@ -60,19 +76,17 @@ std::vector<double> *natural_neighbor(std::vector<Point>& known_coordinates,
             interpolation_values[i] = NULL; //TODO: this is just 0, better way to mark NAN?
         }
     }
+    delete tree;
     return new std::vector<double>(interpolation_values);
 }
 
-typedef boost::geometry::model::point <float, 3, boost::geometry::cs::cartesian> point;
+typedef boost::geometry::model::point <double, 3, boost::geometry::cs::cartesian> point;
 
+#define RAND_MAX 100
 
 int main(int argc, char** argv) {
-    /* point a = point(1,2,3); */
-    /* point b= point(0,0,0); */
-    /* double distance = boost::geometry::distance(a, b); */
-    /* printf("%f\n", distance); */
-
-    int coord_max = 20;
+    printf("%d\n", RAND_MAX);
+    int coord_max = 100;
     int num_known_points = 5000;
     std::vector<Point> known_points(num_known_points);
     std::vector<double> known_values(num_known_points);
@@ -80,6 +94,7 @@ int main(int argc, char** argv) {
         known_points[i] = Point(std::rand(), std::rand(), std::rand());
         known_values[i] = std::rand();
     }
+
     /* interpolation_points */
     std::vector<Point> interpolation_points(coord_max*coord_max*coord_max);
     int idx = 0;
@@ -93,6 +108,9 @@ int main(int argc, char** argv) {
     }
     std::vector<double>* interpolation_values = natural_neighbor(known_points,
                                                                  known_values,
-                                                                 interpolation_points);
-    printf("Completed with %d interpolated values\n", interpolation_values->size());
+                                                                 interpolation_points,
+                                                                 coord_max);
+
+    printf("Completed with %d interpolated values\n", (int) interpolation_values->size());
+    delete interpolation_values;
 }
