1bbc76282a5791353290dfd2ca8c3bc170257245
J. David Giese <johndgiese@gmail.com>
2017-08-13 20:19:30 -0500

    Clean out old visual testing code

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -9,6 +9,6 @@ class TestInputValidation(unittest.TestCase):
     def test_mismatched_num_values_and_points(self):
         points = np.random.rand(100, 3)
         values = np.linalg.norm(points)
-        grid = np.mgrid[0:1:10j, 0:1:10j, 0:1,10j]
+        grid = np.mgrid[0:1:10j, 0:1:10j, 0:1:10j]
         interpolated_values = natural_neighbor(points, values, grid)
         self.assertEqual(interpolated_values.shape, (10, 10, 10))
