0b249115be7f184f7bb57a1b41a690625bbef93c
J. David Giese <johndgiese@gmail.com>
2017-08-19 12:30:58 -0500

    Add partially finished tests

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -5,10 +5,26 @@ import numpy as np
 from naturalneighbor import natural_neighbor
 
 
+def generate_grid(*final_shape):
+    xmax, ymax, zmax = [dim - 1 for dim in final_shape]
+    points = []
+    x_range = np.arange(0, xmax + 1)
+    y_range = np.arange(0, ymax + 1)
+    z_range = np.arange(0, zmax + 1)
+    for x in x_range:
+        for y in y_range:
+            for z in z_range:
+                points.append((x, y, z))
+
+    grid = tuple(zip(*points))
+    return grid
+
+
 class TestInputValidation(unittest.TestCase):
     def test_mismatched_num_values_and_points(self):
         points = np.random.rand(100, 3)
         values = np.linalg.norm(points)
-        grid = np.mgrid[0:1:10j, 0:1:10j, 0:1:10j]
-        interpolated_values = natural_neighbor(points, values, grid)
+        coord_max = (10, 10, 10)
+        grid = generate_grid(*coord_max)
+        interpolated_values = natural_neighbor(points, values, grid, max(coord_max))
         self.assertEqual(interpolated_values.shape, (10, 10, 10))
