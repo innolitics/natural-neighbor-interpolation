d934c5fb0825382dc3575a8db94d5621656f3662
J. David Giese <johndgiese@gmail.com>
2017-08-19 11:34:38 -0500

    Change pointer declaration style
    This is a matter of preference

--- naturalneighbor/_naturalneighbor.cpp
+++ naturalneighbor/_naturalneighbor.cpp
@@ -20,15 +20,17 @@ static struct PyModuleDef module = {
    PyModuleDef_HEAD_INIT,
    "naturalneighbor",
    module_docstring,
-   -1,
+   -1,  // TODO: investigate whether we can support sub-interpreters
    module_methods
 };
 
 PyMODINIT_FUNC PyInit_naturalneighbor(void) {
-    PyObject *m = PyModule_Create(&module);
-    if (m == NULL) { return NULL; }
+    PyObject* m = PyModule_Create(&module);
+    if (m == NULL) {
+        return NULL;
+    }
 
-    import_array(); // Enable numpy functionality
+    import_array();  // Enable numpy functionality
     return m;
 }
 
@@ -36,15 +38,15 @@ typedef geometry::Point<double, 3> Point;
 
 static PyObject* naturalneighbor_natural_neighbor(PyObject* module, PyObject* args) {
     int coord_max;
-    PyObject *known_coord_obj, *known_values_obj, *interpolation_points_obj;
+    PyObject* known_coord_obj,* known_values_obj,* interpolation_points_obj;
 
     if (!PyArg_ParseTuple(args, "OOOi", &known_coord_obj,
                           &known_values_obj, &interpolation_points_obj, &coord_max)) {
         return NULL;
     }
-    PyObject *known_coord_numpy_arr = PyArray_FROM_OTF(known_coord_obj, NPY_DOUBLE, NPY_IN_ARRAY);
-    PyObject *known_values_numpy_arr = PyArray_FROM_OTF(known_values_obj, NPY_DOUBLE, NPY_IN_ARRAY);
-    PyObject *interpolation_points_numpy_arr = PyArray_FROM_OTF(interpolation_points_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+    PyObject* known_coord_numpy_arr = PyArray_FROM_OTF(known_coord_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+    PyObject* known_values_numpy_arr = PyArray_FROM_OTF(known_values_obj, NPY_DOUBLE, NPY_IN_ARRAY);
+    PyObject* interpolation_points_numpy_arr = PyArray_FROM_OTF(interpolation_points_obj, NPY_DOUBLE, NPY_IN_ARRAY);
 
     if (known_coord_numpy_arr == NULL || known_values_numpy_arr == NULL || interpolation_points_numpy_arr == NULL) {
         Py_XDECREF(known_coord_numpy_arr);
@@ -53,9 +55,9 @@ static PyObject* naturalneighbor_natural_neighbor(PyObject* module, PyObject* ar
         return NULL;
     }
 
-    npy_intp *known_coordinates_dims = PyArray_DIMS(known_coord_numpy_arr);
-    double *known_values = (double*)PyArray_GETPTR1(known_values_numpy_arr, 0);
-    npy_intp *interpolation_points_dims = PyArray_DIMS(interpolation_points_numpy_arr);
+    npy_intp* known_coordinates_dims = PyArray_DIMS(known_coord_numpy_arr);
+    double* known_values = (double*)PyArray_GETPTR1(known_values_numpy_arr, 0);
+    npy_intp* interpolation_points_dims = PyArray_DIMS(interpolation_points_numpy_arr);
 
     //TODO: Add in bounds checking if user passes invalid shape
     int num_known_points = known_coordinates_dims[0];
@@ -63,27 +65,27 @@ static PyObject* naturalneighbor_natural_neighbor(PyObject* module, PyObject* ar
     std::vector<double> known_vals(num_known_points);
     for (npy_intp i = 0; i < num_known_points; i++) {
         known_coords[i] = (Point(*(double*)PyArray_GETPTR2(known_coord_numpy_arr, i, 0),
-                                 *(double*)PyArray_GETPTR2(known_coord_numpy_arr, i, 1),
-                                 *(double*)PyArray_GETPTR2(known_coord_numpy_arr, i, 2)));
-        known_vals[i] = *((double*)PyArray_GETPTR1(known_values_numpy_arr, i));
+                                * (double*)PyArray_GETPTR2(known_coord_numpy_arr, i, 1),
+                                * (double*)PyArray_GETPTR2(known_coord_numpy_arr, i, 2)));
+        known_vals[i] =* ((double*)PyArray_GETPTR1(known_values_numpy_arr, i));
     }
 
     int num_interpolation_points = interpolation_points_dims[1];
     std::vector<Point> interp_points(num_interpolation_points);
     for (npy_intp i = 0; i < num_interpolation_points; i++) {
         interp_points[i] = (Point(*(double*)PyArray_GETPTR2(interpolation_points_numpy_arr, 0, i),
-                                  *(double*)PyArray_GETPTR2(interpolation_points_numpy_arr, 1, i),
-                                  *(double*)PyArray_GETPTR2(interpolation_points_numpy_arr, 2, i)));
+                                 * (double*)PyArray_GETPTR2(interpolation_points_numpy_arr, 1, i),
+                                 * (double*)PyArray_GETPTR2(interpolation_points_numpy_arr, 2, i)));
     }
 
-    std::vector<double> *interpolation_values = naturalneighbor::natural_neighbor(known_coords,
+    std::vector<double>* interpolation_values = naturalneighbor::natural_neighbor(known_coords,
                                                                  known_vals,
                                                                  interp_points,
                                                                  coord_max);
     npy_intp dims[] = {num_interpolation_points};
     PyObject* result = PyArray_ZEROS(1, dims, NPY_DOUBLE, 0);
     for (int i = 0; i < num_interpolation_points; i++) {
-        *((double*)PyArray_GETPTR1(result, i)) = (*interpolation_values)[i];
+       * ((double*)PyArray_GETPTR1(result, i)) = (*interpolation_values)[i];
     }
 
     printf("Calculation completed.\n");
