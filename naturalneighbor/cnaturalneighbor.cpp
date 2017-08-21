160d7c7b098161d2918a39014c75f4732eecfadd
Yujan Shrestha <yshrestha@innolitics.com>
2017-08-21 01:11:32 -0500

    Fix index calculation to use roi indices.

--- naturalneighbor/cnaturalneighbor.cpp
+++ naturalneighbor/cnaturalneighbor.cpp
@@ -120,7 +120,7 @@ static PyObject* cnaturalneighbor_griddata(PyObject* self, PyObject* args) {
                             double distance_sq_roi_to_known = deltai_2 + deltaj_2 + deltak_2;
 
                             if (distance_sq_roi_to_known <= distance_sq_query_to_known) {
-                                std::size_t indice = nj*nk*i + nk*j + k;
+                                std::size_t indice = nj*nk*i_roi + nk*j_roi + k_roi;
                                 interp_values_ptr[indice] += nearest_known_point->value;
                                 contribution_counter[indice] += 1;
                             }
